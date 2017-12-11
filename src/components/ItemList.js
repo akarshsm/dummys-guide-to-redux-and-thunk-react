97a78e32ea1b0ea05febfc3118edcd9307a51e62
akarshsm <akarshsm@gmail.com>
2017-12-11 16:58:13 +0530

    Dynamic data through network

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,35 +4,38 @@ class ItemList extends Component {
     constructor() {
         super();
         this.state = {
-            items: [
-                {
-                    id: 1,
-                    label: 'List item 1'
-                },
-                {
-                    id: 2,
-                    label: 'List item 2'
-                },
-                {
-                    id: 3,
-                    label: 'List item 3'
-                },
-                {
-                    id: 4,
-                    label: 'List item 4'
-                }
-            ],
+            items: [],
             isLoading: false,
             hasErrored: false
         };
     }
-    
+
+    fetchData(url) {
+        this.setState({isLoading: true});
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                this.setState({ isLoading: false });
+
+                return response;
+            })
+            .then((response) => response.json())
+            .then((items) => this.setState({items}))
+            .catch(() => this.setState({hasErrored: true}));
+    }
+
+    componentDidMount() {
+        this.fetchData('http://5826ed963900d612000138bd.mockapi.io/items');
+    }
+
     render() {
-        if(this.state.hasErrored){
+        if (this.state.hasErrored) {
             return <p> Sorry Bro! Jack Sparrow hijacked our data.</p>;
         }
 
-        if(this.state.isLoading){
+        if (this.state.isLoading) {
             return <p> Pleae wait! Jack Sparrow is in search...</p>;
         }
 
