88e3220e5aec4805312381ecc2d69a2931ddf6f8
akarshsm <akarshsm@gmail.com>
2017-12-11 19:00:54 +0530

    added Action Creators

---
+++ src/actions/items.js
@@ -0,0 +1,38 @@
+export function itemsHasErrored(bool) {
+    return {
+        type: 'ITEMS_HAS_ERRORED',
+        hasErrored: bool
+    };
+}
+
+export function itmesIsLoading(bool) {
+    return {
+        type: 'ITEMS_IS_LOADING',
+        isLoading: bool
+    };
+}
+
+export function itemsFetchDataSuccess(items) {
+    return {
+        type: 'ITEM_FETCH_DATA_SUCCESS',
+        items
+    };
+}
+
+export function itemsFetchData(url) {
+    return (dispath) => {
+        dispath(itemsIsLoading(true));
+        fetch(url)
+            .then((response) => {
+                if (!response) {
+                    throw Error(response.statusText);
+                }
+                dispath(itemsIsLoading(false));
+
+                return response;
+            })
+            .then((response) => response.json())
+            .then((items) => dispath(itemsFetchDataSuccess(items)))
+            .catch(() => dispath(itemsHasErrored(true)));
+    };
+}
