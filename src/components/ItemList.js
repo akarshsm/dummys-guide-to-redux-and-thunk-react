66b7a421a021216df48c889faf7fe7386004e096
akarshsm <akarshsm@gmail.com>
2017-12-11 21:28:35 +0530

    connetced store state to props

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,56 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { itemsFetchData } from '../actions/items';
 
 class ItemList extends Component {
-    constructor() {
-        super();
-        this.state = {
-            items: [],
-            isLoading: false,
-            hasErrored: false
-        };
-    }
-
-    fetchData(url) {
-        this.setState({isLoading: true});
-        fetch(url)
-            .then((response) => {
-                if (!response.ok) {
-                    throw Error(response.statusText);
-                }
-                this.setState({ isLoading: false });
-
-                return response;
-            })
-            .then((response) => response.json())
-            .then((items) => this.setState({items}))
-            .catch(() => this.setState({hasErrored: true}));
-    }
-
     componentDidMount() {
-        this.fetchData('http://5826ed963900d612000138bd.mockapi.io/items');
+        console.log(' fetch data in did mount ');
+        this.props.fetchData('http://5826ed963900d612000138bd.mockapi.io/items');
     }
 
     render() {
-        if (this.state.hasErrored) {
+        if (this.props.hasErrored) {
             return <p> Sorry Bro! Jack Sparrow hijacked our data.</p>;
         }
 
-        if (this.state.isLoading) {
+        if (this.props.isLoading) {
             return <p> Pleae wait! Jack Sparrow is in search...</p>;
         }
 
         return (
             <ul>
                 {
-                  this.state.items.map((item)=>(
-                    <li key={item.id}>
-                        {item.label}
-                    </li>
-                  ))
+                    this.props.items.map((item)=>(
+                        <li key={item.id}>
+                            {item.label}
+                        </li>
+                    ))
                 }
             </ul>
         );
     }
 }
 
-export default ItemList;
+const mapStateToProps = (state) => {
+    return {
+        items: state.items,
+        hasErrored: state.itemsHasErrored,
+        isLoading: state.itemsIsLoading
+    };
+};
+
+const mapDispathToProps = (dispatch) => {
+    return {
+        fetchData: (url) => dispatch(itemsFetchData(url))
+    };
+};
+
+export default connect(mapStateToProps, mapDispathToProps)(ItemList);
